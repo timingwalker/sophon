31d8f87d1090dfc2de93905ec98b6bee0fb56dd5
timingwalker <huangzh02@pcl.ac.cn>
2024-09-14 15:54:04 +0800

    update software template

---
+++ sw/hello/main.c
@@ -0,0 +1,14 @@
+
+#include <stdint.h>
+
+uintptr_t handle_trap(uintptr_t cause, uintptr_t epc, uintptr_t regs[32])
+{
+    return (epc);
+}
+
+int main()
+{
+    printf("Hello world!\n");
+	return 0;
+}
+
